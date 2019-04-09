4abdeb80e217e18d0f60816440d34db2c223d7a8
pdaniels4143 <pdaniels4143@student.rcschools.net>
2019-04-09 14:04:49 -0500

    Framework for calculator.

--- GUI.py
+++ GUI.py
@@ -1,1 +1,41 @@
+import tkinter as t
 
+class calculator:
+    def __init__(self,masterframe):
+        self.screen=t.Frame(masterframe)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    root=t.Tk()
+    calc=calculator(root)
+    root.mainloop()
+
+main()
